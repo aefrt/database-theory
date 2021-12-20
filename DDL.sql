d203d0e254a18efc033b833dd5003c7b38031266
Копчев Владислав <80166783+aefrt@users.noreply.github.com>
2021-12-20 13:34:25 +0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -26,9 +26,9 @@ CREATE TABLE Sites (
 );  
 
 CREATE TABLE Supplier_categories (  
-    supplier_category_id int PRIMARY KEY,  
+    supplier_category_id int PRIMARY KEY NOT NULL,  
     supplier_parent_category_id int,  
-    app_category_id int REFERENCES Categories(Id),  
+    app_category_id int REFERENCES Categories(Id) ON DELETE CASCADE ON UPDATE CASCADE,  
     site_id int REFERENCES Sites(Id) 
 );  
 
