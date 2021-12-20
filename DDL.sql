c00753da24848696bf8df53e912897aa8aea3d09
Копчев Владислав <80166783+aefrt@users.noreply.github.com>
2021-12-20 13:41:32 +0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -28,8 +28,8 @@ CREATE TABLE Sites (
 CREATE TABLE Supplier_categories (  
     supplier_category_id int PRIMARY KEY NOT NULL,  
     supplier_parent_category_id int,  
-    app_category_id int REFERENCES Categories(Id) ON DELETE CASCADE ON UPDATE CASCADE,  
-    site_id int REFERENCES Sites(Id)  ON DELETE CASCADE ON UPDATE CASCADE
+    app_category_id int REFERENCES Categories(Id) ON DELETE CASCADE ON UPDATE RESTRICT,  
+    site_id int REFERENCES Sites(Id)  ON DELETE CASCADE ON UPDATE RESTRICT
 );  
 
 CREATE TABLE Products (  
@@ -39,9 +39,9 @@ CREATE TABLE Products (
     description text NOT NULL,  
     price int NOT NULL,  
     category_id int REFERENCES Categories(Id),  
-    app_category_id int REFERENCES Supplier_categories(supplier_category_id) ON DELETE CASCADE ON UPDATE CASCADE,  
-    catalog_product_id int REFERENCES Catalog_products(Id) ON DELETE CASCADE ON UPDATE CASCADE,  
-    product_type_id int REFERENCES Product_types(Id) ON DELETE CASCADE ON UPDATE CASCADE
+    app_category_id int REFERENCES Supplier_categories(supplier_category_id) ON DELETE CASCADE ON UPDATE RESTRICT,  
+    catalog_product_id int REFERENCES Catalog_products(Id) ON DELETE CASCADE ON UPDATE RESTRICT,  
+    product_type_id int REFERENCES Product_types(Id) ON DELETE CASCADE ON UPDATE RESTRICT
 );  
 
 CREATE TABLE Experiments_runs (  
@@ -60,6 +60,6 @@ CREATE TABLE Product_site (
 
 CREATE TABLE Product_category (  
     product_id int,  
-    category_id int REFERENCES Categories(Id) ON DELETE CASCADE ON UPDATE CASCADE,  
+    category_id int REFERENCES Categories(Id) ON DELETE CASCADE ON UPDATE RESTRICT,  
     primary key (product_id, category_id)  
 );    
